2ae2976a3b59d7d01ecc4d93611317707b4076a5
Jonathan Wenger <jonathan.wenger@avalara.com>
2024-02-14 12:57:52 -0800

    Update tests for prod.

--- tests/auth/OAuthHelper.spec.ts
+++ tests/auth/OAuthHelper.spec.ts
@@ -1,26 +1,34 @@
-import 'dotenv/config';
-import { Runtime } from '../../src/index';
-import { AvaTaxEnvironment, Configuration } from '../../src/runtime';
-import { OAuthHelper } from '../../src/auth/OAuthHelper';
+import "dotenv/config";
+import { Runtime } from "../../src/index";
+import { AvaTaxEnvironment, Configuration } from "../../src/runtime";
+import { OAuthHelper } from "../../src/auth/OAuthHelper";
 
 const configParams: Runtime.ConfigurationParameters = {
-    appName: 'asv-sdk-test-app',
-    appVersion: '1.0',
-    environment: AvaTaxEnvironment.QA,
-    machineName: 'test-machine',
-    timeout:3000,
-    clientId: process.env.CLIENT_ID,
+  appName: "asv-sdk-test-app",
+  appVersion: "1.0",
+  environment: AvaTaxEnvironment.Production,
+  machineName: "test-machine",
+  timeout: 3000,
+  clientId: process.env.CLIENT_ID,
 };
-const scopes = 'avatax_api';
+const scopes = "avatax_api";
 
-describe('OAuthHelper ', () => {
-    const config = new Configuration(configParams);
+xdescribe("OAuthHelper ", () => {
+  const config = new Configuration(configParams);
 
-    it('should be able to initiate the device authorization flow', async () => {
-        const response = await OAuthHelper.initiateDeviceAuthorizationFlow(scopes, config);
-        expect(response.verificationUrl).toBe('https://ai-awsfqa.avlr.sh/device');
-        const tokenResponse = await OAuthHelper.getAccessTokenForDeviceFlow(response.deviceCode, config);
-        expect(tokenResponse.errorMessage).toBe('authorization_pending');
-        expect(tokenResponse.httpStatusCode).toBe(400);
-    });
+  it("should be able to initiate the device authorization flow", async () => {
+    const response = await OAuthHelper.initiateDeviceAuthorizationFlow(
+      scopes,
+      config
+    );
+    expect(response.verificationUrl).toBe(
+      "https://identity.avalara.com/device"
+    );
+    const tokenResponse = await OAuthHelper.getAccessTokenForDeviceFlow(
+      response.deviceCode,
+      config
+    );
+    expect(tokenResponse.errorMessage).toBe("authorization_pending");
+    expect(tokenResponse.httpStatusCode).toBe(400);
+  });
 });
--- tests/runtime.spec.ts
+++ tests/runtime.spec.ts
@@ -1,100 +1,124 @@
-import type { FetchMockStatic } from 'fetch-mock';
-import 'fetch-mock-jest';
-import fetch from 'node-fetch';
+import type { FetchMockStatic } from "fetch-mock";
+import "fetch-mock-jest";
+import fetch from "node-fetch";
 
-import { Runtime } from '../src/index';
-import { AvaTaxEnvironment, Configuration, FALLBACK_TOKEN_URL } from '../src/runtime';
+import { Runtime } from "../src/index";
+import {
+  AvaTaxEnvironment,
+  Configuration,
+  FALLBACK_TOKEN_URL,
+} from "../src/runtime";
 
-jest.mock(
-  'node-fetch', 
-  () => require('fetch-mock-jest').sandbox(),
-);
-const fetchMock = (fetch as unknown) as FetchMockStatic;
+jest.mock("node-fetch", () => require("fetch-mock-jest").sandbox());
+const fetchMock = fetch as unknown as FetchMockStatic;
 
-describe('Runtime client: ', () => {
-    const qaConfig: Runtime.ConfigurationParameters = {
-        appName: 'asv-sdk-test-app',
-        appVersion: '1.0',
-        environment: AvaTaxEnvironment.QA,
-        machineName: 'test-machine',
-        timeout:3000,
-        clientId: 'someClientId',
-        clientSecret: 'someSecret',
-        testBasePath: 'https://localhost:3000'
-    };
-    const mockTokenResponse = {
-        'access_token': 'fake_access_token', 
-        'expires_in': 3600 
-    };
+describe("Runtime client: ", () => {
+  const qaConfig: Runtime.ConfigurationParameters = {
+    appName: "asv-sdk-test-app",
+    appVersion: "1.0",
+    environment: AvaTaxEnvironment.Production,
+    machineName: "test-machine",
+    timeout: 3000,
+    clientId: "someClientId",
+    clientSecret: "someSecret",
+    testBasePath: "https://localhost:3000",
+  };
+  const mockTokenResponse = {
+    access_token: "fake_access_token",
+    expires_in: 3600,
+  };
 
-    describe('Runtime class', () => {
-        beforeEach(() => fetchMock.reset());
-    
-        it('should be able to applyAuthToRequest for cached token', () =>  {
-            const config = new Configuration(qaConfig);
-            const client = (new Runtime.ApiClient(config) as any);
-            jest.spyOn(client, 'getOAuthAccessToken').mockImplementationOnce(() => ('fake_access_token'))
-            const headerParameters = {};
-            client.applyAuthToRequest(headerParameters, ['OAuth'], 'avatax_api');
-            expect(headerParameters['Authorization']).toBe('Bearer fake_access_token');
-        });
+  describe("Runtime class", () => {
+    beforeEach(() => fetchMock.reset());
 
-        it('should be able to getOAuthAccessToken which is cached', () => {
-            const config = new Configuration(qaConfig);
-            const client = (new Runtime.ApiClient(config) as any);
-            client.accessTokenMap.set('avatax_api', { accessToken: 'fake_access_token', expiry: Math.floor(Date.now() / 1000) + 360 });
-            const token = client.getOAuthAccessToken('avatax_api');
-            expect(token).toBe('fake_access_token');
-        });
-
-        it('should return null when the token is expired', () => {
-            const config = new Configuration(qaConfig);
-            const client = (new Runtime.ApiClient(config) as any);
-            client.accessTokenMap.set('avatax_api', { accessToken: 'fake_access_token', expiry: Math.floor(Date.now() / 1000) + 180 });
-            const token = client.getOAuthAccessToken('avatax_api');
-            expect(token).toBe(null);
-        });
-
-        it('should update an accessToken', async () => {
-            const config = new Configuration(qaConfig);
-            const client = (new Runtime.ApiClient(config) as any);
-            
-            jest.spyOn(client, 'buildOAuthRequest').mockImplementationOnce(() => (mockTokenResponse));
-        
-            await client.updateOAuthAccessToken('avatax_api');
-            expect(client.accessTokenMap.get('avatax_api')).toEqual(expect.objectContaining({
-                accessToken: 'fake_access_token'
-            }));
-        });
-
-        it('should be able to buildOAuthRequest for a given set of scopes', async () => {
-            const mockTokenEndpoint = 'https://ai.avalara.com/token';
-            fetchMock.get('https://ai-awsfqa.avlr.sh/.well-known/openid-configuration', { "token_endpoint": mockTokenEndpoint });
-            fetchMock.post(mockTokenEndpoint, mockTokenResponse)
-            const config = new Configuration(qaConfig);
-            const client = (new Runtime.ApiClient(config) as any);
-            const tokenData = await client.buildOAuthRequest();
-            expect(tokenData).toEqual(mockTokenResponse);
-        });
+    it("should be able to applyAuthToRequest for cached token", () => {
+      const config = new Configuration(qaConfig);
+      const client = new Runtime.ApiClient(config) as any;
+      jest
+        .spyOn(client, "getOAuthAccessToken")
+        .mockImplementationOnce(() => "fake_access_token");
+      const headerParameters = {};
+      client.applyAuthToRequest(headerParameters, ["OAuth"], "avatax_api");
+      expect(headerParameters["Authorization"]).toBe(
+        "Bearer fake_access_token"
+      );
     });
-    
 
-    describe('Configuration class', () => {
-        beforeEach(() => fetchMock.reset());
-
-        it('should be able to getTokenUrl', async () => {
-            
-            fetchMock.get('https://ai-awsfqa.avlr.sh/.well-known/openid-configuration', { "token_endpoint": "https://ai.avalara.com/token"});
-            const config = new Configuration(qaConfig);
-            await config.setOIDCUrls();
-            expect(config.oidcUrls.tokenUrl).toBe('https://ai.avalara.com/token');
-        });
-
-        it('should use fallback token url if OIDC document does not return.', async () => {
-            fetchMock.get('https://ai-awsfqa.avlr.sh/.well-known/openid-configuration', { throws: { message: 'endpoint unavailable'} } );
-            const config = new Configuration(qaConfig);
-            await config.setOIDCUrls();
-            expect(config.oidcUrls.tokenUrl).toBe(FALLBACK_TOKEN_URL);
-        });
+    it("should be able to getOAuthAccessToken which is cached", () => {
+      const config = new Configuration(qaConfig);
+      const client = new Runtime.ApiClient(config) as any;
+      client.accessTokenMap.set("avatax_api", {
+        accessToken: "fake_access_token",
+        expiry: Math.floor(Date.now() / 1000) + 360,
+      });
+      const token = client.getOAuthAccessToken("avatax_api");
+      expect(token).toBe("fake_access_token");
     });
+
+    it("should return null when the token is expired", () => {
+      const config = new Configuration(qaConfig);
+      const client = new Runtime.ApiClient(config) as any;
+      client.accessTokenMap.set("avatax_api", {
+        accessToken: "fake_access_token",
+        expiry: Math.floor(Date.now() / 1000) + 180,
+      });
+      const token = client.getOAuthAccessToken("avatax_api");
+      expect(token).toBe(null);
+    });
+
+    it("should update an accessToken", async () => {
+      const config = new Configuration(qaConfig);
+      const client = new Runtime.ApiClient(config) as any;
+
+      jest
+        .spyOn(client, "buildOAuthRequest")
+        .mockImplementationOnce(() => mockTokenResponse);
+
+      await client.updateOAuthAccessToken("avatax_api");
+      expect(client.accessTokenMap.get("avatax_api")).toEqual(
+        expect.objectContaining({
+          accessToken: "fake_access_token",
+        })
+      );
+    });
+
+    it("should be able to buildOAuthRequest for a given set of scopes", async () => {
+      const mockTokenEndpoint = "https://ai.avalara.com/token";
+      fetchMock.get(
+        "https://identity.avalara.com/.well-known/openid-configuration",
+        { token_endpoint: mockTokenEndpoint }
+      );
+      fetchMock.post(mockTokenEndpoint, mockTokenResponse);
+      const config = new Configuration(qaConfig);
+      const client = new Runtime.ApiClient(config) as any;
+      const tokenData = await client.buildOAuthRequest();
+      expect(tokenData).toEqual(mockTokenResponse);
+    });
+  });
+
+  describe("Configuration class", () => {
+    beforeEach(() => fetchMock.reset());
+
+    it("should be able to getTokenUrl", async () => {
+      fetchMock.get(
+        "https://identity.avalara.com/.well-known/openid-configuration",
+        { token_endpoint: "https://identity.avalara.com/connect/token" }
+      );
+      const config = new Configuration(qaConfig);
+      await config.setOIDCUrls();
+      expect(config.oidcUrls.tokenUrl).toBe(
+        "https://identity.avalara.com/connect/token"
+      );
+    });
+
+    it("should use fallback token url if OIDC document does not return.", async () => {
+      fetchMock.get(
+        "https://identity.avalara.com/.well-known/openid-configuration",
+        { throws: { message: "endpoint unavailable" } }
+      );
+      const config = new Configuration(qaConfig);
+      await config.setOIDCUrls();
+      expect(config.oidcUrls.tokenUrl).toBe(FALLBACK_TOKEN_URL);
+    });
+  });
 });
